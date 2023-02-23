b0c152df5337143113962e82f93c0c093073edeb
Karin Melo <karinsantosmelo@gmail.com>
2023-02-23 05:13:44 -0300

    Validando produtos

--- src/Routes/productsRoute.ts
+++ src/Routes/productsRoute.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import productsController from '../Controllers/productsController';
-import validationProduct from '../middlewares/productMiddleware';
+import { validationAmount, validationName } from '../middlewares/productMiddleware';
 
 const productsRoute = express.Router();
-productsRoute.post('/', validationProduct, productsController.product);
+productsRoute.post('/', validationName, validationAmount, productsController.product);
 productsRoute.get('/', productsController.getAllProduct);
 
 export default productsRoute;
--- src/middlewares/productMiddleware.ts
+++ src/middlewares/productMiddleware.ts
@@ -1,13 +1,46 @@
 import { NextFunction, Request, Response } from 'express';
 
-const validationProduct = (request: Request, response: Response, next: NextFunction) => {
-  const { name, amount } = request.body;
-  if (!name || !amount) {
+const validationName = (request: Request, response: Response, next: NextFunction) => {
+  const { name } = request.body;
+  if (!name) {
     return response.status(400).json({
-      message: 'Some required fields are missing',
+      message: '"name" is required',
+    });
+  }
+  if (name.length < 3) {
+    return response.status(422).json({
+      message: '"name" length must be at least 3 characters long',
+    });
+  }
+  if (typeof name !== 'string') {
+    return response.status(422).json({
+      message: '"name" must be a string',
+    });
+  }
+  return next();
+};
+
+const validationAmount = (request: Request, response: Response, next: NextFunction) => {
+  const { amount } = request.body;
+  if (!amount) {
+    return response.status(400).json({
+      message: '"amount" is required',
+    });
+  }
+  if (amount.length < 3) {
+    return response.status(422).json({
+      message: '"amount" length must be at least 3 characters long',
+    });
+  }
+  if (typeof amount !== 'string') {
+    return response.status(422).json({
+      message: '"amount" must be a string',
     });
   }
   return next();
 };
   
-export default validationProduct;
+export {
+  validationName,
+  validationAmount,
+};
